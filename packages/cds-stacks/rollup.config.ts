b76e12c462623308c3d4cee1371fd1016e257491
Guillaume 'Yugo' Coguiec <guillaume@logical.work>
2023-03-01 10:59:19 -0500

    wip: Setup the cds-stacks package.

---
+++ packages/cds-stacks/rollup.config.ts
@@ -0,0 +1,49 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import json from '@rollup/plugin-json';
+
+import pkg from './package.json' assert { type: 'json' };
+
+const external = [
+  ...Object.keys(pkg.peerDependencies),
+  /^@cdktf\/provider-aws\/.*/
+];
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: 'dist/index.mjs',
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      json(),
+      esbuild({
+        include: /\.ts$/,
+        sourceMap: true,
+        minify: true,
+        target: 'esnext',
+        tsconfig: 'tsconfig.build.json'
+      })
+    ],
+    external
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+      sourcemap: false
+    },
+    external,
+    plugins: [dts()]
+  }
+];
